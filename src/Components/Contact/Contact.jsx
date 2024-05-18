4ec4faf607690078580e80dc29bfb118f8043762
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-18 19:34:04 +0600

    nested route add and fixed to home

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+
+
+const Contact = () => {
+    return (
+        <div>
+           <h2>Contact me</h2> 
+           <p>Plese contact for any info thanks</p>
+        </div>
+    );
+};
+
+export default Contact;
--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,30 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/about",
-    element: <div>about me page</div>,
-  },
-  {
-    path: "/contuct",
-    element: <div>contuct details</div>,
+    element: <Home></Home>,
+    children:[
+     {
+      path: "/about",
+      element: <About></About>,
+     },
+     {
+      path: "/contact",
+      element: <Contact></Contact>,
+     }
+    ]
   },
 ]);
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+ <RouterProvider router={router} />
   </React.StrictMode>,
 )
